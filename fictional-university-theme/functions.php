f8815cc0a8ce0b97c9c8c6760d43591abf3b4157
Michael Gibbons <michael@btownweb.com>
2020-02-05 12:18:29 -0700

    Added program post type related to event post type

--- fictional-university-theme/functions.php
+++ fictional-university-theme/functions.php
@@ -30,6 +30,11 @@ function university_adjust_queries($query){
     );
 
   }
+  if(!is_admin() AND is_post_type_archive('program') AND $query -> is_main_query()){
+    $query -> set('order','ASC');
+    $query -> set('orderby', 'title');
+    $query -> set('posts_per_page', -1);
+  }
   
 
 }
